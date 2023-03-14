7318884b47dd57d5c110064f63fb3f1435543046
Akhilesh-Chandrakar <akhil79022@gmail.com>
2023-03-14 17:29:37 +0530

    completed project in one go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import "./App.css"
 
 function App() {
+  //hooks for deciding hover or not hover
+const [focus, setFocus] = useState(false);
+//hooks for setting position
+const [position, setPosition] = useState("top")
+
+const hoverIn=()=>{
+setFocus(true);
+}
+const hoverOut=()=>{
+  setFocus(false);
+  }
+  const visible={display:focus?"block":"none"}
+  function handleClick(e){
+
+    setPosition(e.target.value
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tooltip</h1>
+      <div className='btns'>
+        <div className='btn'><button onClick={handleClick} value="top">Top</button></div>
+        <div className='btn'><button onClick={handleClick} value="bottom">Bottom</button></div>
+        <div className='btn'><button onClick={handleClick} value="right">Right</button></div>
+        <div className='btn'><button onClick={handleClick} value="left">Left</button></div>
+      </div>
+      <div className='hover'>
+        <button onMouseOver={hoverIn} onMouseOut={hoverOut}>Hover over me</button>
+        <div className={position} style={visible}>Thanks for Hovering! I am in {position} tooltip</div>
+      </div>
     </div>
   );
 }
