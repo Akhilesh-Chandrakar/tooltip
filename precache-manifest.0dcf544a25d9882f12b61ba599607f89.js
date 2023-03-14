cf23d2d41035be0e86eb82b597272093f05b29ce
Akhilesh-Chandrakar <akhil79022@gmail.com>
2023-03-14 17:43:02 +0530

    Updates

---
+++ precache-manifest.0dcf544a25d9882f12b61ba599607f89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bea082f25aa878242417ad83e3b32ad",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "dd616485da62bd1dab01",
+    "url": "/tooltip/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6b9a2e2379cc953b0d89",
+    "url": "/tooltip/static/css/main.884a7be9.chunk.css"
+  },
+  {
+    "revision": "dd616485da62bd1dab01",
+    "url": "/tooltip/static/js/2.7db54112.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip/static/js/2.7db54112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9a2e2379cc953b0d89",
+    "url": "/tooltip/static/js/main.b4b69436.chunk.js"
+  },
+  {
+    "revision": "6d81586e19be32d511a4",
+    "url": "/tooltip/static/js/runtime-main.ca1d1748.js"
+  }
+]);
